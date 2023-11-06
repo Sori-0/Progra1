e4985098849ed18b9554d70b71b9c9261b8280da
Sori-0 <osoriano@cocoschool.mx>
2023-11-06 10:49:39 -0600

    Agregue Sprites y Vida

--- Assets/Sripts/ShowLife.cs
+++ Assets/Sripts/ShowLife.cs
@@ -7,19 +7,15 @@ public class ShowLife : MonoBehaviour
 {
     private TMP_Text textComponent;
     private Spawn spawnScript;
-    private int life = 3;
     void Start()
     {
         spawnScript = FindObjectOfType<Spawn>();
+        textComponent = GetComponent<TMP_Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
         textComponent.text = "Vidas: " + spawnScript.life;
-    }
-    public void SeeLife()
-    {
-        //spawnScript.life;    
     }
 }
